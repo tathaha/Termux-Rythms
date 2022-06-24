e3e628fc4c8a75f105fafb323053c1d55e187a3c
Chiraitori <77224364+tathaha@users.noreply.github.com>
2022-06-24 18:19:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ pkg install wget
 echo install adb
 curl -s https://raw.githubusercontent.com/rendiix/termux-adb-fastboot/master/install.sh | bash
 echo get backup file
-wget https://download1322.mediafire.com/jflehd8tzhgg/k2yueqy06cnnhwv/phigros.ab
+wget https://github.com/tathaha/Termux-phigros/raw/main/phigros.ab
 clear
 echo please do this command
 echo follow this link https://pastebin.com/cKBQJ5MJ
