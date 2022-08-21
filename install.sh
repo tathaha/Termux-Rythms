91d8a036a4ca3dbe3f97a571112f04ff3429c192
Chiraitori <77224364+tathaha@users.noreply.github.com>
2022-08-21 08:34:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,37 @@ echo install wget
 pkg install wget -y
 echo install adb
 pkg install android-tools -y
-echo get backup file
-wget https://github.com/tathaha/Termux-phigros/raw/main/phigros.ab
 clear
+echo "What game you want to to get data"
+echo "You Can Choose At Least 1 Option Otherwise The script will broken"
+echo "Phigros (y/n)"
+read phigros
+if [[ $phigros ==  "y"]]; then
+    echo "you choose this option right, so don't choose orther option"
+    wget https://github.com/tathaha/Termux-phigros/raw/main/phigros.ad
+    d=phigros
+esle
+    echo "choose an orther option"
+fi
+echo "Muse Dash (y/n)"
+echo "This allow for musedash from Google Play"
+read musedash
+if [[ $musedash ==  "y"]]; then
+    echo "you choose this option right, so don't choose orther option"
+    wget https://github.com/tathaha/Termux-phigros/raw/main/musedash.ab
+    d=musedash
+esle
+    echo "choose an orther option"
+fi
+echo "Orzmic (y/n)"
+read orzmic
+if [[ $orzmic ==  "y"]]; then
+    echo "you choose this option right, so don't choose orther option"
+    wget https://github.com/tathaha/Termux-phigros/raw/main/orzmic.ab
+    d=orzmic.ab
+esle
+    echo "this is end of option backup the file"
+fi
 echo "Enter Yor Ip"
 read a
 echo "Enter Yor Port Pair"
@@ -26,5 +54,5 @@ adb pair "$a":"$b"
 echo "Enter Yor Port Connect"
 read c
 adb connect "$a":"$c"
-ech restore ab file
-adb restore phigros.ab
+echo restore ab file
+adb restore $d
